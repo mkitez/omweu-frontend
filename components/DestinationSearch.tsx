0aebcf1153c556fd7a9ee7d3fe36081fb41687a3
mkitez <user.pr@mail.ru>
2022-10-08 12:25:57 +0200

    Trip search

---
+++ components/DestinationSearch.tsx
@@ -0,0 +1,96 @@
+import { useState } from 'react';
+import type { FormEventHandler } from 'react';
+import { debounce } from '../utils/commonUtils';
+import { API_URL } from '../utils/constants';
+
+interface Destination {
+  id: string;
+  name: string;
+  country: {
+    name: string;
+  };
+}
+
+const DestinationSearch = () => {
+  const [date, setDate] = useState('');
+  const [originOptions, setOriginOptions] = useState<Destination[]>([]);
+  const [destOptions, setDestOptions] = useState<Destination[]>([]);
+  const [selectedOrigin, selectOrigin] = useState('');
+  const [selectedDest, selectDest] = useState('');
+
+  const destSearch = async (
+    query: string,
+    callback: typeof setOriginOptions
+  ) => {
+    const response = await fetch(
+      `${API_URL}/destinations/search?query=${query}`
+    );
+    const responseJson = await response.json();
+    callback(responseJson);
+  };
+  const debouncedSearch = debounce(destSearch, 500);
+  const handleOriginInput: FormEventHandler<HTMLInputElement> = async (e) => {
+    const value = e.currentTarget.value;
+    debouncedSearch(value, setOriginOptions);
+  };
+  const handleDestInput: FormEventHandler<HTMLInputElement> = async (e) => {
+    const value = e.currentTarget.value;
+    debouncedSearch(value, setDestOptions);
+  };
+
+  const handleSubmit: FormEventHandler = async (e) => {
+    e.preventDefault();
+    const response = await fetch(
+      `${API_URL}/trips/search/?origin=${selectedOrigin}&dest=${selectedDest}&date=${date}`
+    );
+    const responseJson = await response.json();
+    console.log(responseJson);
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          From:
+          <input type="text" onInput={handleOriginInput}></input>
+        </label>
+        <select
+          value={selectedOrigin}
+          onChange={(e) => selectOrigin(e.target.value)}
+        >
+          <option value=""></option>
+          {originOptions.map((origin) => (
+            <option key={origin.id} value={origin.id}>
+              {origin.name}
+            </option>
+          ))}
+        </select>
+        <label>
+          To:
+          <input type="text" onInput={handleDestInput}></input>
+        </label>
+        <select
+          value={selectedDest}
+          onChange={(e) => selectDest(e.target.value)}
+        >
+          <option value=""></option>
+          {destOptions.map((dest) => (
+            <option key={dest.id} value={dest.id}>
+              {dest.name}
+            </option>
+          ))}
+        </select>
+        <label>
+          Date:
+          <input
+            value={date}
+            type="text"
+            onInput={(e) => setDate(e.currentTarget.value)}
+          ></input>
+        </label>
+        <input type="submit" value="Search" />
+      </form>
+    </div>
+  );
+};
+
+export default DestinationSearch;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Link from 'next/link';
+import DestinationSearch from '../components/DestinationSearch';
 import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
@@ -12,6 +13,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Link href="/login">Login</Link>
+      <DestinationSearch />
     </div>
   );
 };
