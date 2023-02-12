8549e0169a33d6e84f4efa08df15e6fe3b495ff3
mkitez <user.pr@mail.ru>
2023-02-12 19:20:48 +0100

    base url

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,2 +1,7 @@
-export const API_URL = 'http://localhost:8000/api/v1';
+const BASE_URL =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:8000'
+    : 'https://omweu.app';
+
+export const API_URL = `${BASE_URL}/api/v1`;
 export const HERE_API_KEY = process.env.NEXT_PUBLIC_HERE_API_KEY;
