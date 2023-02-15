909efe329e4980b93c625f82fc08c2a517fd72de
mkitez <user.pr@mail.ru>
2023-02-15 22:22:34 +0100

    deployment process update

---
+++ setup_build_env.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+echo NEXT_PUBLIC_HERE_API_KEY=$HERE_API_KEY >> .env.local
+echo VK_CLIENT_ID=$VK_CLIENT_ID >> .env.local
+echo VK_CLIENT_SECRET=$VK_CLIENT_SECRET >> .env.local
+echo NEXTAUTH_URL=$NEXTAUTH_URL >> .env.local
+echo NEXTAUTH_SECRET=$NEXTAUTH_SECRET >> .env.local
+echo NEXT_PUBLIC_HERE_API_KEY=$HERE_API_KEY >> .env.local
