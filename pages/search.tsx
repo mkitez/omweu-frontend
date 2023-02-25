6efc0e1726d3aaa44b9aac430748222cb3869c01
mkitez <user.pr@mail.ru>
2023-02-25 19:09:39 +0100

    search results as cards

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,9 +1,12 @@
 import useSWR from 'swr';
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
+import Link from 'next/link';
 import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { Card } from 'antd';
 import { API_URL } from '../utils/constants';
+import type { Trip } from '../components/Trips';
 
 const Search = () => {
   const router = useRouter();
@@ -37,13 +40,18 @@ const Search = () => {
   }
 
   return (
-    <ul>
-      {data.results.map((trip: any) => (
-        <li key={trip.id}>
-          {trip.origin.name} - {trip.dest.name}
-        </li>
+    <div>
+      {data.results.map((trip: Trip) => (
+        <Link href={`/trips/${trip.id}`} key={trip.id}>
+          <Card hoverable style={{ marginBottom: 10 }}>
+            <Card.Meta
+              title={`${trip.origin.name} - ${trip.dest.name}`}
+              description={trip.date}
+            />
+          </Card>
+        </Link>
       ))}
-    </ul>
+    </div>
   );
 };
 
