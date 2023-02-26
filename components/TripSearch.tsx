fd898c40c6f744a68b1beea5b3877c58ecc25356
mkitez <user.pr@mail.ru>
2023-02-27 00:01:58 +0100

    search widget on results page

--- components/TripSearch.tsx
+++ components/TripSearch.tsx
@@ -4,29 +4,47 @@ import { Form, Button } from 'antd';
 import PlaceInput from './PlaceInput';
 import DateTimeInput from './DateTimeInput';
 import type { FormData } from './TripEditForm';
+import dayjs from 'dayjs';
 
 const TripSearch = () => {
   const router = useRouter();
   const { t } = useTranslation('common');
 
+  const { from_input, to_input, from, to, date } = router.query;
+  const initialValues = {
+    from: from_input ? { label: from_input, value: from } : undefined,
+    to: to_input ? { label: to_input, value: to } : undefined,
+    date: date ? dayjs(date as string, 'YYYY-MM-DD') : undefined,
+  };
+
   const onFinish = async (formData: FormData) => {
-    const date = formData.date.format('YYYY-MM-DD');
+    const { from, to, date } = formData;
+    const formattedDate = date.format('YYYY-MM-DD');
     router.push(
-      `/search?from=${formData.from.value}&to=${formData.to.value}&date=${date}`
+      `/search?from=${from.value}&to=${to.value}&date=${formattedDate}&from_input=${from.label}&to_input=${to.label}`,
+      undefined,
+      { shallow: true }
     );
   };
 
   return (
-    <Form onFinish={onFinish} layout="inline" requiredMark={false}>
-      <PlaceInput name="from" label="From" />
-      <PlaceInput name="to" label="To" />
-      <DateTimeInput name="date" label="Date" />
-      <Form.Item>
-        <Button type="primary" htmlType="submit">
-          {t('search')}
-        </Button>
-      </Form.Item>
-    </Form>
+    <div style={{ marginBottom: 15 }}>
+      <Form
+        onFinish={onFinish}
+        layout="inline"
+        requiredMark={false}
+        initialValues={initialValues}
+      >
+        <PlaceInput name="from" label="From" />
+        <PlaceInput name="to" label="To" />
+        <DateTimeInput name="date" label="Date" />
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            {t('search')}
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
   );
 };
 
--- pages/search.tsx
+++ pages/search.tsx
@@ -6,6 +6,7 @@ import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { Card } from 'antd';
 import { API_URL } from '../utils/constants';
 import type { Trip } from '../components/Trips';
+import TripSearch from '../components/TripSearch';
 
 const Search = () => {
   const router = useRouter();
@@ -20,30 +21,30 @@ const Search = () => {
     }
   );
 
+  let searchResults;
   if (isLoading) {
-    return <div>Searching...</div>;
-  }
-
-  if (error) {
-    return <div>Error: {error}</div>;
-  }
-
-  if (data.results.length === 0) {
-    return <div>No results</div>;
+    searchResults = <div>Searching...</div>;
+  } else if (error) {
+    searchResults = <div>Error: {error}</div>;
+  } else if (data.results.length === 0) {
+    searchResults = <div>No results</div>;
+  } else {
+    searchResults = data.results.map((trip: Trip) => (
+      <Link href={`/trips/${trip.id}`} key={trip.id}>
+        <Card hoverable style={{ marginBottom: 10 }}>
+          <Card.Meta
+            title={`${trip.origin.name} - ${trip.dest.name}`}
+            description={trip.date}
+          />
+        </Card>
+      </Link>
+    ));
   }
 
   return (
     <div>
-      {data.results.map((trip: Trip) => (
-        <Link href={`/trips/${trip.id}`} key={trip.id}>
-          <Card hoverable style={{ marginBottom: 10 }}>
-            <Card.Meta
-              title={`${trip.origin.name} - ${trip.dest.name}`}
-              description={trip.date}
-            />
-          </Card>
-        </Link>
-      ))}
+      <TripSearch />
+      {searchResults}
     </div>
   );
 };
