c4ca9a52d8cc5484f800cdf30b74bc247056490b
mkitez <user.pr@mail.ru>
2023-03-19 17:00:06 +0100

    minute step validation

--- components/DateTimeInput.tsx
+++ components/DateTimeInput.tsx
@@ -1,5 +1,6 @@
 import dayjs from 'dayjs';
 import { DatePicker, Form } from 'antd';
+import { Rule } from 'antd/es/form';
 
 interface Props {
   name: string;
@@ -9,16 +10,31 @@ interface Props {
 
 const DateTimeInput = ({ name, label, showTime }: Props) => {
   const format = showTime ? 'YYYY/MM/DD HH:mm' : 'YYYY/MM/DD';
+  const rules: Rule[] = [{ required: true, message: 'Please select a date' }];
+  if (showTime) {
+    rules.push({
+      async validator(_, value: dayjs.Dayjs) {
+        if (![0, 30].includes(value.minute())) {
+          throw Error('Please use a 30 minute interval');
+        }
+      },
+    });
+  }
+
   return (
-    <Form.Item
-      name={name}
-      label={label}
-      rules={[{ required: true, message: 'Please select a date' }]}
-    >
+    <Form.Item name={name} label={label} rules={rules}>
       <DatePicker
         allowClear={false}
         format={format}
-        showTime={showTime ? { minuteStep: 30, format: 'HH:mm' } : false}
+        showTime={
+          showTime
+            ? {
+                minuteStep: 30,
+                format: 'HH:mm',
+                defaultValue: dayjs().startOf('hour'),
+              }
+            : false
+        }
         showNow={false}
         disabledDate={(current) => current && current < dayjs().startOf('day')}
       />
