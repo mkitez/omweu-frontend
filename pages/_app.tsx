2bb91a1810df94ed608720edc1b424346c516899
mkitez <user.pr@mail.ru>
2023-03-19 23:16:02 +0100

    adjust locale detection

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { ReactElement, ReactNode } from 'react';
 import type { NextPage } from 'next';
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
 import { SessionProvider } from 'next-auth/react';
 import { Session } from 'next-auth';
 import { ConfigProvider } from 'antd';
@@ -9,11 +10,12 @@ import AppLayout from '../components/AppLayout';
 import '../styles/globals.css';
 import 'antd/dist/reset.css';
 
-import localeRu from 'antd/locale/ru_RU';
+import type { Locale } from 'antd/lib/locale';
+import ru from 'antd/locale/ru_RU';
 import 'dayjs/locale/ru';
 
-const locales: Record<string, typeof localeRu> = {
-  ru: localeRu,
+const locales: Record<string, Locale> = {
+  ru,
 };
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
@@ -30,11 +32,12 @@ type AppPropsWithLayout = AppProps &
   };
 
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  const { locale } = useRouter();
   const getLayout =
     Component.getLayout ?? ((page) => <AppLayout>{page}</AppLayout>);
   return (
     <SessionProvider session={pageProps.session}>
-      <ConfigProvider locale={locales[pageProps._nextI18Next?.initialLocale]}>
+      <ConfigProvider locale={locales[locale as string]}>
         {getLayout(<Component {...pageProps} />)}
       </ConfigProvider>
     </SessionProvider>
