07a049196b3ba614dffbe771a7911e4dfea728f8
mkitez <user.pr@mail.ru>
2023-05-04 19:11:51 +0200

    banner

---
+++ components/Banner.tsx
@@ -0,0 +1,26 @@
+import Image from 'next/image';
+import { Montserrat } from 'next/font/google';
+import bannerImg from '../assets/bannerImg.svg';
+import styles from '../styles/Banner.module.css';
+
+const montserrat = Montserrat({
+  subsets: ['latin', 'cyrillic'],
+});
+
+const Banner = () => (
+  <div className={styles.container}>
+    <div className={styles.banner}>
+      <div className={styles.bannerContainer}>
+        <Image src={bannerImg} alt="eubycar.com banner" />
+      </div>
+      <div className={styles.textContainer}>
+        <h1 className={montserrat.className}>
+          <span className={styles.highlight}>Экономьте</span> на поездках в
+          Европу
+        </h1>
+      </div>
+    </div>
+  </div>
+);
+
+export default Banner;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { useTranslation } from 'next-i18next';
 import Head from 'next/head';
+import Banner from '../components/Banner';
 import TripSearch from '../components/TripSearch';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import type { InferGetServerSidePropsType } from 'next';
@@ -17,7 +18,7 @@ const Home = (
         <meta name="description" content="omw EU" />
         <link rel="icon" href="/favicon.png" />
       </Head>
-      <h1>{t('welcomeMessage')}</h1>
+      <Banner />
       <TripSearch />
     </div>
   );
