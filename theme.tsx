5db589fa3ca7165ef4709af1b9049be434be4689
mkitez <user.pr@mail.ru>
2023-05-06 14:19:50 +0200

    open sans font

--- theme.tsx
+++ theme.tsx
@@ -2,6 +2,8 @@ import type { ThemeConfig } from 'antd/es/config-provider/context';
 
 const theme: ThemeConfig = {
   token: {
+    fontFamily:
+      "-apple-system,BlinkMacSystemFont,'Open Sans',sans-serif,Arial,'Apple Color Emoji','Segoe UI Emoji','Segoe UI Symbol','Noto Color Emoji'",
     fontSize: 16,
     colorText: '#151515',
     colorPrimary: '#F65959',
