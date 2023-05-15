0ee329bf81bff0298a3d526811dc3754a3f5cc90
mkitez <user.pr@mail.ru>
2023-05-15 22:40:17 +0200

    trip details styles

--- pages/dashboard/trips.tsx
+++ pages/dashboard/trips.tsx
@@ -48,6 +48,7 @@ export const getServerSideProps: GetServerSideProps = async ({
   const translations = await serverSideTranslations(locale as string, [
     'common',
     'dashboard',
+    'trip',
   ]);
 
   return {
--- pages/trips/[tripId].tsx
+++ pages/trips/[tripId].tsx
@@ -7,6 +7,7 @@ import { Session } from 'next-auth';
 import api from '../../services/api';
 import AuthService from '../../services/auth.service';
 import { getServerSideProps } from '../dashboard/trips';
+import styles from '../../styles/Trip.module.css';
 
 const Trip = ({ session }: { session: Session }) => {
   const router = useRouter();
@@ -25,28 +26,56 @@ const Trip = ({ session }: { session: Session }) => {
   );
 
   if (isLoading) {
-    return <div>{t('loading', { ns: 'common' })}</div>;
+    return <div className="content">{t('loading', { ns: 'common' })}</div>;
   }
 
   if (error) {
-    return <div>{t('errors.common', { ns: 'common' })}</div>;
+    return (
+      <div className="content">{t('errors.common', { ns: 'common' })}</div>
+    );
   }
 
   return (
-    <div className="content">
+    <div className={`content ${styles.root}`}>
       <h1>{t('tripDetails')}</h1>
-      <Descriptions title={`${data.origin.name} - ${data.dest.name}`}>
-        <Descriptions.Item>
+      <Descriptions
+        title={
+          <>
+            {data.origin.name} &mdash; {data.dest.name}
+          </>
+        }
+        layout="vertical"
+      >
+        <Descriptions.Item label={t('tripDate')}>
           {new Date(data.date).toLocaleString(i18n.language)}
         </Descriptions.Item>
-        <Descriptions.Item>
-          <Image
-            src={data.driver.photo}
-            width={20}
-            height={20}
-            alt="driver photo"
-          />{' '}
-          {data.driver.first_name}
+        <Descriptions.Item label={t('driver')}>
+          <div className={styles.driver}>
+            <div className={styles.imgContainer}>
+              <Image
+                src={data.driver.photo}
+                width={100}
+                height={100}
+                alt="driver photo"
+              />
+            </div>
+            <div>{data.driver.first_name}</div>
+          </div>
+        </Descriptions.Item>
+        <Descriptions.Item label={t('contacts')}>
+          <div>
+            {data.driver.phone_number && (
+              <>
+                <span>
+                  {t('phone')}: {data.driver.phone_number}
+                </span>
+                <br />
+              </>
+            )}
+            {data.driver.telegram_username && (
+              <span>Telegram: {data.driver.telegram_username}</span>
+            )}
+          </div>
         </Descriptions.Item>
       </Descriptions>
     </div>
