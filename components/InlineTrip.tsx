415428fd8ef6314a9a832809ffc5a3e9bc4189f0
mkitez <user.pr@mail.ru>
2023-05-30 00:09:08 +0200

    inline trip component

---
+++ components/InlineTrip.tsx
@@ -0,0 +1,47 @@
+import { FC } from 'react';
+import Image from 'next/image';
+import type { Trip } from '../components/Trips';
+import styles from '../styles/InlineTrip.module.css';
+import { useTranslation } from 'next-i18next';
+
+const TripLine = () => {
+  return (
+    <div className={styles.tripLine}>
+      <div className={styles.circle}></div>
+      <div className={styles.box}></div>
+      <div className={styles.circle}></div>
+    </div>
+  );
+};
+
+interface Props {
+  trip: Trip;
+}
+
+const InlineTrip: FC<Props> = ({ trip }) => {
+  const { i18n } = useTranslation('common');
+  const tripTime = new Date(trip.date).toLocaleTimeString(i18n.language, {
+    timeStyle: 'short',
+  });
+  return (
+    <div className={styles.root}>
+      <div className={styles.tripDetails}>
+        <div className={styles.time}>{tripTime}</div>
+        <TripLine />
+        <div className={styles.destContainer}>
+          <div className={styles.origin}>{trip.origin.name}</div>
+          <div className={styles.dest}>{trip.dest.name}</div>
+        </div>
+        <div className={styles.price}>&euro;{trip.price}</div>
+      </div>
+      <div className={styles.driver}>
+        <div className={styles.imgContainer}>
+          <Image src={trip.driver.photo} width={100} height={100} alt="" />
+        </div>
+        <div className={styles.driverName}>{trip.driver.first_name}</div>
+      </div>
+    </div>
+  );
+};
+
+export default InlineTrip;
