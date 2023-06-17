a0beaf6fd51a97bacd8b3fbdb683c87e200fb729
mkitez <user.pr@mail.ru>
2023-06-17 17:57:03 +0200

    update hostname

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,7 +1,7 @@
 const BASE_URL =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:8000'
-    : 'https://omweu.app';
+    : 'https://eubycar.com';
 
 export const API_URL = `${BASE_URL}/api/v1`;
 export const HERE_API_KEY = process.env.NEXT_PUBLIC_HERE_API_KEY;
