f42107d571a2bfc025d0698833031c93e1ba4267
mkitez <user.pr@mail.ru>
2024-01-07 14:01:47 +0100

    faq page

---
+++ pages/faq.tsx
@@ -0,0 +1,48 @@
+import Head from 'next/head';
+import Markdown from 'react-markdown';
+import cmsApi from '../services/cmsApi';
+import { GetStaticProps, InferGetStaticPropsType } from 'next';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { SSRConfig } from 'next-i18next';
+
+const FAQ = ({
+  title,
+  content,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
+  return (
+    <div className="container">
+      <Head>
+        <title>{`${title} | EUbyCar.com`}</title>
+      </Head>
+      <h1>{title}</h1>
+      <Markdown>{content}</Markdown>
+    </div>
+  );
+};
+
+type Props = {
+  title: string;
+  content: string;
+} & SSRConfig;
+
+export const getStaticProps: GetStaticProps<Props> = async ({ locale }) => {
+  const translations = await serverSideTranslations(locale as string, [
+    'common',
+  ]);
+
+  const response = await cmsApi.get('faq', {
+    params: { locale },
+  });
+  const { title, content } = response.data.data.attributes;
+
+  return {
+    revalidate: 60,
+    props: {
+      ...translations,
+      title,
+      content,
+    },
+  };
+};
+
+export default FAQ;
--- utils/constants.ts
+++ utils/constants.ts
@@ -2,7 +2,10 @@ const BASE_URL =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:8000'
     : 'https://eubycar.com';
+const CMS_HOST_NAME = process.env.STRAPI_CONTAINER_NAME || '0.0.0.0';
 
 export const API_URL = `${BASE_URL}/api/v1`;
+export const CMS_API_URL = `http://${CMS_HOST_NAME}:1337/api`;
 export const HERE_API_KEY = process.env.NEXT_PUBLIC_HERE_API_KEY;
 export const RECAPTCHA_SITE_KEY = process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY;
+export const STRAPI_ACCESS_TOKEN = process.env.STRAPI_ACCESS_TOKEN;
