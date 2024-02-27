5d5d5ed6191e72b2b66e636763304248430538a1
mkitez <user.pr@mail.ru>
2024-02-27 17:51:03 +0100

    help text

--- components/DriverPreferencesFormFields.tsx
+++ components/DriverPreferencesFormFields.tsx
@@ -15,65 +15,72 @@ const DriverPreferencesFormFields: React.FC<Props> = ({ data }) => {
   const { t } = useTranslation(['dashboard', 'common']);
   const [showFields, setShowFields] = useState(false);
 
-  if (data === null && !showFields) {
-    return (
-      <Row>
-        <Button
-          type="dashed"
-          icon={<PlusOutlined />}
-          onClick={() => setShowFields(true)}
-        >
-          {t('driver_preferences.add_button_label')}
-        </Button>
-      </Row>
-    );
-  }
-
-  const parentField = 'driver_preferences';
-  const booleanFields: (keyof DriverPreferences)[] = [
-    'smoking_allowed',
-    'can_deliver',
-    'food_allowed',
-    'max_two_on_backseat',
-    'music_allowed',
-    'pets_allowed',
-  ];
   return (
-    <>
-      <Row gutter={20}>
-        {booleanFields.map((fieldName) => (
-          <Col key={fieldName} md={12}>
+    <div className={styles.driverPreferences}>
+      <div className={styles.helpText}>{t('driver_preferences.help_text')}</div>
+      {(() => {
+        if (data === null && !showFields) {
+          return (
+            <Row>
+              <Button
+                type="dashed"
+                icon={<PlusOutlined />}
+                onClick={() => setShowFields(true)}
+              >
+                {t('driver_preferences.add_button_label')}
+              </Button>
+            </Row>
+          );
+        }
+
+        const parentField = 'driver_preferences';
+        const booleanFields: (keyof DriverPreferences)[] = [
+          'smoking_allowed',
+          'can_deliver',
+          'food_allowed',
+          'max_two_on_backseat',
+          'music_allowed',
+          'pets_allowed',
+        ];
+        return (
+          <>
+            <Row gutter={20}>
+              {booleanFields.map((fieldName) => (
+                <Col key={fieldName} md={12}>
+                  <Item
+                    label={t(`driver_preferences.labels.${fieldName}`)}
+                    name={[parentField, fieldName]}
+                    valuePropName="checked"
+                    labelCol={{ span: 20 }}
+                    wrapperCol={{ span: 4 }}
+                    labelAlign="left"
+                  >
+                    <Switch />
+                  </Item>
+                </Col>
+              ))}
+            </Row>
             <Item
-              label={t(`driver_preferences.labels.${fieldName}`)}
-              name={[parentField, fieldName]}
-              valuePropName="checked"
-              labelCol={{ span: 20 }}
-              wrapperCol={{ span: 4 }}
+              name={[parentField, 'gender']}
+              label={t('driver_preferences.labels.gender')}
               labelAlign="left"
             >
-              <Switch />
+              <Radio.Group>
+                <Radio value={null}>
+                  {t('driver_preferences.gender_values.irrelevant')}
+                </Radio>
+                <Radio value={false}>
+                  {t('driver_preferences.gender_values.female')}
+                </Radio>
+                <Radio value={true}>
+                  {t('driver_preferences.gender_values.male')}
+                </Radio>
+              </Radio.Group>
             </Item>
-          </Col>
-        ))}
-      </Row>
-      <Item
-        name={[parentField, 'gender']}
-        label={t('driver_preferences.labels.gender')}
-        labelAlign="left"
-      >
-        <Radio.Group>
-          <Radio value={null}>
-            {t('driver_preferences.gender_values.irrelevant')}
-          </Radio>
-          <Radio value={false}>
-            {t('driver_preferences.gender_values.female')}
-          </Radio>
-          <Radio value={true}>
-            {t('driver_preferences.gender_values.male')}
-          </Radio>
-        </Radio.Group>
-      </Item>
-    </>
+          </>
+        );
+      })()}
+    </div>
   );
 };
 
