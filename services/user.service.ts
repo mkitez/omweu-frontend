30564d8aef229aa1d0e031e50861022b53788fed
mkitez <user.pr@mail.ru>
2024-05-18 18:52:32 +0200

    update user service

--- services/user.service.ts
+++ services/user.service.ts
@@ -2,14 +2,12 @@ import { UserFormData } from '../components/UserProfileForm';
 import BaseService from './baseService';
 
 class UserService extends BaseService {
-  async getSelf() {
-    const response = await this.api.get('/users/self/');
-    return response.data;
+  getSelf() {
+    return this.api.get('/users/self/');
   }
 
-  async updateSelf(data: Partial<UserFormData>) {
-    const response = await this.api.put('/users/self/', data);
-    return response.data;
+  updateSelf(data: Partial<UserFormData>) {
+    return this.api.put('/users/self/', data);
   }
 
   uploadPhoto(data: FormData) {
