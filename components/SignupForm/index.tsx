6c355fde24d0b2d9c7129086005cca7f890b1eea
mkitez <user.pr@mail.ru>
2024-05-30 10:34:37 +0200

    41: Signup form quick fix

--- components/SignupForm/index.tsx
+++ components/SignupForm/index.tsx
@@ -94,229 +94,227 @@ const SignupForm: React.FC<Props> = ({ onSuccess }) => {
   };
 
   return (
-    <>
-      <Form
-        form={form}
-        onFinish={onFinish}
-        className={styles.root}
-        labelCol={{ span: 8 }}
-        wrapperCol={{ span: 16 }}
+    <Form
+      form={form}
+      onFinish={onFinish}
+      className={styles.root}
+      labelCol={{ span: 8 }}
+      wrapperCol={{ span: 16 }}
+    >
+      <Form.Item
+        name="email"
+        label={t('registration.email')}
+        rules={[
+          { required: true, message: t('errors.enterEmail') as string },
+          { type: 'email' },
+        ]}
+        {...getValidationProps('email')}
       >
-        <Form.Item
-          name="email"
-          label={t('registration.email')}
-          rules={[
-            { required: true, message: t('errors.enterEmail') as string },
-            { type: 'email' },
-          ]}
-          {...getValidationProps('email')}
-        >
-          <Input
-            placeholder={t('registration.email') || ''}
-            onFocus={() =>
-              setValidationErrors((errors) => ({ ...errors, email: undefined }))
-            }
-          />
-        </Form.Item>
-        <Form.Item
-          name="password"
-          label={t('registration.password')}
-          rules={[
-            { required: true, message: t('errors.enterPassword') as string },
-            {
-              min: 8,
-              message: t('errors.shortPassword', { len: 8 }) as string,
-            },
-          ]}
-          {...getValidationProps('password')}
-        >
-          <Input.Password
-            placeholder={t('registration.password') || ''}
-            onFocus={() =>
-              setValidationErrors((errors) => ({
-                ...errors,
-                password: undefined,
-              }))
-            }
-          />
-        </Form.Item>
-        <Form.Item
-          name="password_confirmation"
-          label={t('registration.confirmPassword')}
-          rules={[
-            { required: true, message: t('errors.enterPassword') as string },
-            ({ getFieldValue }) => ({
-              validator(_, value) {
-                if (!value || getFieldValue('password') === value) {
-                  return Promise.resolve();
-                }
-                return Promise.reject(
-                  new Error(t('errors.passwordsDoNotMatch') as string)
-                );
-              },
-            }),
-          ]}
-          dependencies={['password']}
-        >
-          <Input.Password placeholder={t('registration.password') || ''} />
-        </Form.Item>
-        <Divider />
-        <Form.Item
-          name="first_name"
-          label={t('registration.firstName')}
-          rules={[
-            { required: true, message: t('errors.enterName') as string },
-            { max: 30, message: t('errors.longName') as string },
-          ]}
-        >
-          <Input placeholder={t('registration.firstName') || ''} />
-        </Form.Item>
-        <Form.Item
-          name="last_name"
-          label={t('registration.lastName')}
-          rules={[{ max: 30, message: t('errors.longLastName') as string }]}
-        >
-          <Input placeholder={t('registration.lastName') || ''} />
-        </Form.Item>
-        <Form.Item
-          name="birth_date"
-          label={t('registration.birthDate')}
-          rules={[
-            () => ({
-              validator(_, value) {
-                if (!value) {
-                  return Promise.resolve();
-                }
-                const date = dayjs(value, dateFormats[i18n.language], true);
-                if (!date.isValid()) {
-                  return Promise.reject(
-                    new Error(
-                      `${t('errors.dateFormat')}: ${t('registration.birthDateFormat')}`
-                    )
-                  );
-                }
-                const age = calculateAge(value);
-                if (age < 18) {
-                  return Promise.reject(
-                    new Error(t('errors.dateValueLow') as string)
-                  );
-                }
-                if (age > 100) {
-                  return Promise.reject(
-                    new Error(t('errors.dateValueHigh') as string)
-                  );
-                }
+        <Input
+          placeholder={t('registration.email') || ''}
+          onFocus={() =>
+            setValidationErrors((errors) => ({ ...errors, email: undefined }))
+          }
+        />
+      </Form.Item>
+      <Form.Item
+        name="password"
+        label={t('registration.password')}
+        rules={[
+          { required: true, message: t('errors.enterPassword') as string },
+          {
+            min: 8,
+            message: t('errors.shortPassword', { len: 8 }) as string,
+          },
+        ]}
+        {...getValidationProps('password')}
+      >
+        <Input.Password
+          placeholder={t('registration.password') || ''}
+          onFocus={() =>
+            setValidationErrors((errors) => ({
+              ...errors,
+              password: undefined,
+            }))
+          }
+        />
+      </Form.Item>
+      <Form.Item
+        name="password_confirmation"
+        label={t('registration.confirmPassword')}
+        rules={[
+          { required: true, message: t('errors.enterPassword') as string },
+          ({ getFieldValue }) => ({
+            validator(_, value) {
+              if (!value || getFieldValue('password') === value) {
                 return Promise.resolve();
+              }
+              return Promise.reject(
+                new Error(t('errors.passwordsDoNotMatch') as string)
+              );
+            },
+          }),
+        ]}
+        dependencies={['password']}
+      >
+        <Input.Password placeholder={t('registration.password') || ''} />
+      </Form.Item>
+      <Divider />
+      <Form.Item
+        name="first_name"
+        label={t('registration.firstName')}
+        rules={[
+          { required: true, message: t('errors.enterName') as string },
+          { max: 30, message: t('errors.longName') as string },
+        ]}
+      >
+        <Input placeholder={t('registration.firstName') || ''} />
+      </Form.Item>
+      <Form.Item
+        name="last_name"
+        label={t('registration.lastName')}
+        rules={[{ max: 30, message: t('errors.longLastName') as string }]}
+      >
+        <Input placeholder={t('registration.lastName') || ''} />
+      </Form.Item>
+      <Form.Item
+        name="birth_date"
+        label={t('registration.birthDate')}
+        rules={[
+          () => ({
+            validator(_, value) {
+              if (!value) {
+                return Promise.resolve();
+              }
+              const date = dayjs(value, dateFormats[i18n.language], true);
+              if (!date.isValid()) {
+                return Promise.reject(
+                  new Error(
+                    `${t('errors.dateFormat')}: ${t('registration.birthDateFormat')}`
+                  )
+                );
+              }
+              const age = calculateAge(value);
+              if (age < 18) {
+                return Promise.reject(
+                  new Error(t('errors.dateValueLow') as string)
+                );
+              }
+              if (age > 100) {
+                return Promise.reject(
+                  new Error(t('errors.dateValueHigh') as string)
+                );
+              }
+              return Promise.resolve();
+            },
+          }),
+          { required: true, message: t('errors.enterBirthDate') as string },
+        ]}
+        validateTrigger={['onBlur']}
+      >
+        <Input placeholder={t('registration.birthDateFormat') as string} />
+      </Form.Item>
+      <Divider />
+      <Form.Item
+        name="phone_number"
+        label={t('registration.phone')}
+        rules={[
+          { pattern: /\+\d+/, message: t('errors.numberFormat') as string },
+          { max: 20, message: t('errors.longNumber') as string },
+          { required: true, message: t('errors.enterNumber') as string },
+        ]}
+        {...getValidationProps('phone_number')}
+      >
+        <Input
+          placeholder={t('registration.phone') || ''}
+          onFocus={() =>
+            setValidationErrors((errors) => ({
+              ...errors,
+              phone_number: undefined,
+            }))
+          }
+        />
+      </Form.Item>
+      {showExtraFields ? (
+        <>
+          <Form.Item
+            name="whatsapp_number"
+            label={t('registration.whatsapp')}
+            rules={[
+              {
+                pattern: /\+\d+/,
+                message: t('errors.numberFormat') as string,
               },
-            }),
-            { required: true, message: t('errors.enterBirthDate') as string },
-          ]}
-          validateTrigger={['onBlur']}
-        >
-          <Input placeholder={t('registration.birthDateFormat') as string} />
-        </Form.Item>
-        <Divider />
-        <Form.Item
-          name="phone_number"
-          label={t('registration.phone')}
-          rules={[
-            { pattern: /\+\d+/, message: t('errors.numberFormat') as string },
-            { max: 20, message: t('errors.longNumber') as string },
-            { required: true, message: t('errors.enterNumber') as string },
-          ]}
-          {...getValidationProps('phone_number')}
-        >
-          <Input
-            placeholder={t('registration.phone') || ''}
-            onFocus={() =>
-              setValidationErrors((errors) => ({
-                ...errors,
-                phone_number: undefined,
-              }))
-            }
-          />
-        </Form.Item>
-        {showExtraFields ? (
-          <>
-            <Form.Item
-              name="whatsapp_number"
-              label={t('registration.whatsapp')}
-              rules={[
-                {
-                  pattern: /\+\d+/,
-                  message: t('errors.numberFormat') as string,
-                },
-                { max: 20, message: t('errors.longNumber') as string },
-              ]}
-              {...getValidationProps('whatsapp_number')}
-            >
-              <Input
-                placeholder={t('registration.whatsapp') || ''}
-                onFocus={() =>
-                  setValidationErrors((errors) => ({
-                    ...errors,
-                    whatsapp_number: undefined,
-                  }))
-                }
-              />
+              { max: 20, message: t('errors.longNumber') as string },
+            ]}
+            {...getValidationProps('whatsapp_number')}
+          >
+            <Input
+              placeholder={t('registration.whatsapp') || ''}
+              onFocus={() =>
+                setValidationErrors((errors) => ({
+                  ...errors,
+                  whatsapp_number: undefined,
+                }))
+              }
+            />
+          </Form.Item>
+          <Form.Item
+            name="telegram_username"
+            label={t('registration.telegram')}
+            {...getValidationProps('telegram_username')}
+          >
+            <Input
+              placeholder={t('registration.telegram_placeholder') || ''}
+              onFocus={() =>
+                setValidationErrors((errors) => ({
+                  ...errors,
+                  telegram_username: undefined,
+                }))
+              }
+            />
+          </Form.Item>
+        </>
+      ) : (
+        <Row>
+          <Col sm={{ offset: 8 }}>
+            <Form.Item>
+              <Button
+                type="dashed"
+                icon={<PlusOutlined />}
+                onClick={() => setShowExtraFields(true)}
+              >
+                {t('registration.show_extra_contacts')}
+              </Button>
             </Form.Item>
-            <Form.Item
-              name="telegram_username"
-              label={t('registration.telegram')}
-              {...getValidationProps('telegram_username')}
-            >
-              <Input
-                placeholder={t('registration.telegram_placeholder') || ''}
-                onFocus={() =>
-                  setValidationErrors((errors) => ({
-                    ...errors,
-                    telegram_username: undefined,
-                  }))
-                }
-              />
-            </Form.Item>
-          </>
-        ) : (
-          <Row>
-            <Col sm={{ offset: 8 }}>
-              <Form.Item>
-                <Button
-                  type="dashed"
-                  icon={<PlusOutlined />}
-                  onClick={() => setShowExtraFields(true)}
-                >
-                  {t('registration.show_extra_contacts')}
-                </Button>
-              </Form.Item>
-            </Col>
-          </Row>
-        )}
-        <Form.Item
-          name="captcha"
-          initialValue={null}
-          rules={[
-            { required: true, message: t('errors.solveCaptcha') as string },
-          ]}
-          wrapperCol={{ xs: 24, sm: { offset: 8 } }}
-          {...getValidationProps('captcha')}
-        >
-          <ReCAPTCHA
-            sitekey={RECAPTCHA_SITE_KEY as string}
-            onChange={(value) => form.setFieldValue('captcha', value)}
-            className={styles.captchaContainer}
-            hl={i18n.language}
-          />
-        </Form.Item>
-        <Form.Item
-          className={styles.submitButtonContainer}
-          wrapperCol={{ span: 24 }}
-        >
-          <Button type="primary" htmlType="submit" loading={loading}>
-            {t('registration.formButtonLabel')}
-          </Button>
-        </Form.Item>
-      </Form>
-    </>
+          </Col>
+        </Row>
+      )}
+      <Form.Item
+        name="captcha"
+        initialValue={null}
+        rules={[
+          { required: true, message: t('errors.solveCaptcha') as string },
+        ]}
+        wrapperCol={{ xs: 24, sm: { offset: 8 } }}
+        {...getValidationProps('captcha')}
+      >
+        <ReCAPTCHA
+          sitekey={RECAPTCHA_SITE_KEY as string}
+          onChange={(value) => form.setFieldValue('captcha', value)}
+          className={styles.captchaContainer}
+          hl={i18n.language}
+        />
+      </Form.Item>
+      <Form.Item
+        className={styles.submitButtonContainer}
+        wrapperCol={{ span: 24 }}
+      >
+        <Button type="primary" htmlType="submit" loading={loading}>
+          {t('registration.formButtonLabel')}
+        </Button>
+      </Form.Item>
+    </Form>
   );
 };
 
