970ca735d0c2c394fb054f5302ee5f0d6b97bac0
mkitez <user.pr@mail.ru>
2024-05-31 10:07:35 +0200

    fix swap button on mobile

--- components/PlaceInput/PlaceInputSearch.tsx
+++ components/PlaceInput/PlaceInputSearch.tsx
@@ -1,7 +1,8 @@
-import { ReactNode, forwardRef } from 'react';
 import { Select, SelectProps } from 'antd';
-import { useTranslation } from 'next-i18next';
 import type { DefaultOptionType, RefSelectProps } from 'antd/es/select';
+import { useTranslation } from 'next-i18next';
+import { forwardRef, ReactNode } from 'react';
+
 import { useHereAutocomplete } from '../../hooks/useHereAutocomplete';
 import styles from './PlaceInputSearch.module.css';
 
@@ -10,49 +11,48 @@ interface Props extends SelectProps {
   swapBtn?: ReactNode;
 }
 
-const PlaceInputSearch = forwardRef<RefSelectProps, Props>(function PlaceInput(
-  props,
-  ref
-) {
-  const { i18n } = useTranslation();
-  const { suggestions, getSuggestions } = useHereAutocomplete({
-    lang: i18n.language,
-  });
+const PlaceInputSearch = forwardRef<RefSelectProps, Props>(
+  function PlaceInput(props, ref) {
+    const { i18n } = useTranslation();
+    const { suggestions, getSuggestions } = useHereAutocomplete({
+      lang: i18n.language,
+    });
 
-  const handleSearch = (newValue: string) => {
-    getSuggestions(newValue);
-  };
+    const handleSearch = (newValue: string) => {
+      getSuggestions(newValue);
+    };
 
-  const { icon, swapBtn, ...restProps } = props;
-  let className = styles.input;
-  if (swapBtn) {
-    className += ` ${styles.inputWithSwap}`;
+    const { icon, swapBtn, ...restProps } = props;
+    let className = styles.input;
+    if (swapBtn) {
+      className += ` ${styles.inputWithSwap}`;
+    }
+
+    return (
+      <div className={styles.root}>
+        {icon && <div className={styles.icon}>{icon}</div>}
+        <Select
+          showSearch
+          defaultActiveFirstOption
+          filterOption={false}
+          onSearch={handleSearch}
+          showArrow={false}
+          notFoundContent={null}
+          labelInValue={true}
+          options={suggestions.map(
+            (place): DefaultOptionType => ({
+              value: place.id,
+              label: place.address.label,
+            })
+          )}
+          className={className}
+          ref={ref}
+          {...restProps}
+        />
+        {swapBtn && <div className={styles.swap}>{swapBtn}</div>}
+      </div>
+    );
   }
-
-  return (
-    <div className={styles.root}>
-      {icon && <div className={styles.icon}>{icon}</div>}
-      <Select
-        showSearch
-        defaultActiveFirstOption
-        filterOption={false}
-        onSearch={handleSearch}
-        showArrow={false}
-        notFoundContent={null}
-        labelInValue={true}
-        options={suggestions.map(
-          (place): DefaultOptionType => ({
-            value: place.id,
-            label: place.address.label,
-          })
-        )}
-        className={className}
-        ref={ref}
-        {...restProps}
-      />
-      {swapBtn && <div className={styles.swap}>{swapBtn}</div>}
-    </div>
-  );
-});
+);
 
 export default PlaceInputSearch;
