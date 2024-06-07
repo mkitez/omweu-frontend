74a439239fb338399d4f4832d22b6d4267568850
mkitez <user.pr@mail.ru>
2024-06-07 14:48:05 +0200

    WIP: subscription create form

---
+++ components/SubscriptionEditForm/index.tsx
@@ -0,0 +1,121 @@
+import { Button, Col, DatePicker, Form, message, Row } from 'antd';
+import { DefaultOptionType } from 'antd/es/select';
+import axios from 'axios';
+import dayjs from 'dayjs';
+import { useTranslation } from 'next-i18next';
+import { useRouter } from 'next/router';
+import { useState } from 'react';
+
+import { useSubscriptionApi } from '../../hooks/api/useSubscriptionsApi';
+import { PlaceInputEdit } from '../PlaceInput';
+
+interface SubscriptionFormData {
+  origin: DefaultOptionType;
+  destination: DefaultOptionType;
+  start_date: dayjs.Dayjs;
+  end_date: dayjs.Dayjs;
+}
+
+const SubscriptionEditForm: React.FC<{}> = () => {
+  const { t } = useTranslation(['dashboard', 'common']);
+  const router = useRouter();
+  const [form] = Form.useForm<SubscriptionFormData>();
+  const api = useSubscriptionApi();
+
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async (data: SubscriptionFormData) => {
+    const { origin, destination, start_date, end_date } = data;
+    if (!origin.value || !destination.value) {
+      return;
+    }
+    setLoading(true);
+    const dateFormat = 'YYYY-MM-DD';
+    try {
+      await api.createSubscription({
+        origin_id: String(origin.value),
+        destination_id: String(destination.value),
+        start_date: start_date.format(dateFormat),
+        end_date: end_date.format(dateFormat),
+      });
+    } catch (e) {
+      if (axios.isAxiosError(e)) {
+        message.error(t('errors.common', { ns: 'common' }));
+      }
+    }
+    setLoading(false);
+  };
+
+  return (
+    <Form
+      form={form}
+      requiredMark={false}
+      onFinish={handleSubmit}
+      labelCol={{ span: 4 }}
+      wrapperCol={{ span: 16 }}
+    >
+      <Form.Item
+        name="origin"
+        label={t('subscriptions.labels.origin')}
+        rules={[{ required: true }]}
+      >
+        <PlaceInputEdit placeholder={t('subscriptions.labels.origin')} />
+      </Form.Item>
+      <Form.Item
+        name="destination"
+        label={t('subscriptions.labels.destination')}
+        rules={[{ required: true }]}
+      >
+        <PlaceInputEdit placeholder={t('subscriptions.labels.destination')} />
+      </Form.Item>
+      <Form.Item
+        name="start_date"
+        label={t('subscriptions.labels.start_date')}
+        rules={[{ required: true }]}
+      >
+        <DatePicker
+          allowClear={false}
+          inputReadOnly
+          disabledDate={(current) =>
+            current &&
+            (current < dayjs().startOf('day') ||
+              current > dayjs().add(1, 'year'))
+          }
+          format="ll"
+        />
+      </Form.Item>
+      <Form.Item
+        name="end_date"
+        label={t('subscriptions.labels.end_date')}
+        rules={[{ required: true }]}
+      >
+        <DatePicker
+          allowClear={false}
+          inputReadOnly
+          disabledDate={(current) => {
+            if (!current) {
+              return false;
+            }
+            const startDate = form.getFieldValue('start_date');
+            return (
+              current < startDate ||
+              current > startDate.add(3, 'day').startOf('day')
+            );
+          }}
+          format="ll"
+        />
+      </Form.Item>
+      <Row gutter={[10, 10]}>
+        <Col lg={{ offset: 4 }}>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" loading={loading}>
+              {t('subscriptions.create')}
+            </Button>
+          </Form.Item>
+        </Col>
+      </Row>
+    </Form>
+  );
+};
+
+export default SubscriptionEditForm;
---
+++ pages/new-subscription.tsx
@@ -0,0 +1,56 @@
+import { App } from 'antd';
+import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
+import { unstable_getServerSession } from 'next-auth';
+import { useTranslation } from 'next-i18next';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+
+import SubscriptionEditForm from '../components/SubscriptionEditForm';
+import { useSubscriptionApi } from '../hooks/api/useSubscriptionsApi';
+import styles from '../styles/NewCar.module.css';
+import { NextPageWithLayout } from './_app';
+import { authOptions } from './api/auth/[...nextauth]';
+
+type PageProps = InferGetServerSidePropsType<typeof getServerSideProps>;
+
+const NewSubscriptionPage: NextPageWithLayout<PageProps> = () => {
+  const { t } = useTranslation('dashboard');
+  const router = useRouter();
+  const { message } = App.useApp();
+  const api = useSubscriptionApi();
+
+  return (
+    <div className="container">
+      <Head>
+        <title>{`${t('subscriptions.create_title')} | EUbyCar.com`}</title>
+      </Head>
+      <div className={styles.root}>
+        <h1>{t('subscriptions.create_title')}</h1>
+        <SubscriptionEditForm />
+      </div>
+    </div>
+  );
+};
+
+NewSubscriptionPage.auth = true;
+
+export const getServerSideProps: GetServerSideProps = async ({
+  req,
+  res,
+  locale,
+}) => {
+  const session = await unstable_getServerSession(req, res, authOptions);
+  const translations = await serverSideTranslations(locale as string, [
+    'common',
+    'dashboard',
+  ]);
+  return {
+    props: {
+      ...translations,
+      session,
+    },
+  };
+};
+
+export default NewSubscriptionPage;
