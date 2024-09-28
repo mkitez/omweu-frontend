10f1a62bce3ff28da9f499b035254bece76b8bb1
mkitez <user.pr@mail.ru>
2024-09-28 16:10:13 +0200

    no chats logic

--- components/ChatList/index.tsx
+++ components/ChatList/index.tsx
@@ -1,3 +1,6 @@
+import { SearchOutlined } from '@ant-design/icons';
+import { Button } from 'antd';
+import { useTranslation } from 'next-i18next';
 import Link from 'next/link';
 
 import { Chat } from '../../services/chat.service';
@@ -10,6 +13,21 @@ interface Props {
 }
 
 const ChatList: React.FC<Props> = ({ data: chats }) => {
+  const { t } = useTranslation('dashboard');
+
+  if (chats.length === 0) {
+    return (
+      <div className={styles.noChats}>
+        <p>{t('chats.noChats')}</p>
+        <p>
+          <Link href="/?search=true" legacyBehavior passHref>
+            <Button icon={<SearchOutlined />}>{t('chats.searchTrips')}</Button>
+          </Link>
+        </p>
+      </div>
+    );
+  }
+
   return (
     <div>
       {chats.map((chat) => (
